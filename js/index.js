461a5481a5dd5cdac0e3c89256dd7b5d77c000fe
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-10-09 21:22:59 -0300

    update

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var catalogoServico = [
+    {
+        nome: "Lazer", 
+        diaria: 50.00
+    }, 
+    {
+        nome: "Banho", 
+        diaria: 540.00
+    },
+    {
+        nome: "Tosa", 
+        diaria: 510.00
+    },
+    {
+        nome: "Hospedagem", 
+        valor: 10.00
+    }
+];
+
+for (var servicos of catalogoServico) {
+    var cardServicos = `
+    <div class="card-product">
+    <div class="card-image"></div>
+    <div class="card-desc">
+        ${servicos.nome}<br>
+        R$${servicos.diaria}
+    </div>
+    <div class="card-button-add">
+        <button class="button-add" type="button"> adicionar </button>
+    </div>
+</div>`;
+
+document.getElementById("container-product").innerHTML += cardServicos;
+}
